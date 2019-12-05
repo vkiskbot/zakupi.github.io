55789815e566f9f73fc7f13aa3c404c1842cb1ce
vkiskbot <53118918+vkiskbot@users.noreply.github.com>
2019-12-05 16:07:56 +0300

    Add files via upload

---
+++ precache-manifest.140d2fca6f77bd4a0a9f0e0a55a4fc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f428cbcdf43e23b52fa92462894bffcf",
+    "url": "/vkiskbot/zakupi.github.io.git/index.html"
+  },
+  {
+    "revision": "9be40e8408ed3fdd3379",
+    "url": "/vkiskbot/zakupi.github.io.git/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "2ae022afee86584b1ac4",
+    "url": "/vkiskbot/zakupi.github.io.git/static/css/main.4d181329.chunk.css"
+  },
+  {
+    "revision": "9be40e8408ed3fdd3379",
+    "url": "/vkiskbot/zakupi.github.io.git/static/js/2.c33d1468.chunk.js"
+  },
+  {
+    "revision": "2ae022afee86584b1ac4",
+    "url": "/vkiskbot/zakupi.github.io.git/static/js/main.c4b74ba0.chunk.js"
+  },
+  {
+    "revision": "05006984e4615c89a85c",
+    "url": "/vkiskbot/zakupi.github.io.git/static/js/runtime-main.69470644.js"
+  }
+]);
